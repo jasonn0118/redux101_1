4655d6012db8eb3a12884c1e530c3bdc32ed5b8a
Jasonn0118 <tlswoals2006@gmail.com>
2019-12-19 11:53:44 -0800

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const increment = () => {
+  return {
+    type: "INCREMENT",
+    // payload: number
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: "DECREMENT",
+    // payload: number
+  };
+};
